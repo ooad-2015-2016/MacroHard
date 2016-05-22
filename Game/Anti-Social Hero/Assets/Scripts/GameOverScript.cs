ea95276e6c901e47b69d79c54ac4b3218a79c9ba
Delmond <muha.delalic@hotmail.com>
2016-05-22 19:15:42 +0200

    opis dodatne klase

---
+++ Game/Anti-Social Hero/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+public class GameOverScript : MonoBehaviour
+{
+	void OnGUI()
+	{
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
+
+		if (
+			GUI.Button(
+				new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(1 * Screen.height / 3) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+				),
+				"Retry!"
+			)
+		)
+		{
+			if (Application.loadedLevelName == "Stage1")
+			Application.LoadLevel ("Stage1");
+			else
+				Application.LoadLevel ("Stage2");
+		}
+
+		if (
+			GUI.Button(
+				new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(2 * Screen.height / 3) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+				),
+				"Back to menu"
+			)
+		)
+		{
+			Application.LoadLevel("Menu");
+		}
+	}
+}
---
+++ Game/Anti-Social Hero/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+	private GUISkin skin;
+
+	void Start()
+	{
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}
+
+	void OnGUI()
+	{
+		const int buttonWidth = 138;
+		const int buttonHeight = 70;
+
+		GUI.skin = skin;
+
+		Rect buttonRect = new Rect(
+			2*Screen.width / 3 - (1*buttonWidth/3)+20,
+			(Screen.height / 3) - (buttonHeight /2)-10,
+			buttonWidth,
+			buttonHeight
+		);
+		Rect buttonRect2 = new Rect(
+			2*Screen.width / 3 - (1*buttonWidth/3)+20,
+			(2*Screen.height / 3) - (buttonHeight )-40,
+			buttonWidth,
+			buttonHeight
+		);
+		Rect buttonRect3 = new Rect(
+			2*Screen.width / 3 - (1*buttonWidth/3)+20,
+			(Screen.height) - (buttonHeight )-100,
+			buttonWidth,
+			buttonHeight
+		);
+
+		if(GUI.Button(buttonRect,"Normal mode"))
+		{
+			Application.LoadLevel("Stage1");
+
+		}
+		if(GUI.Button(buttonRect2,"Dubstep mode"))
+		{
+			Application.LoadLevel("Stage2");
+
+		}
+		if(GUI.Button(buttonRect3,"Exit"))
+		{
+			Application.Quit();
+		}
+	}
+	void Update() {
+		if (Input.GetKey("escape"))
+			Application.Quit();
+
+	}
+}
