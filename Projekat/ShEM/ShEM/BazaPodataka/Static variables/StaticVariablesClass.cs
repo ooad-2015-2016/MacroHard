edbbb89395da8279a314faa0f7c02f728654c6e5
united4ever <eceric31@gmail.com>
2016-05-22 12:20:12 +0200

    Jos malo commitanja

---
+++ Projekat/ShEM/ShEM/BazaPodataka/Static variables/StaticVariablesClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShEM.BazaPodataka.Static_variables
+{
+    public static class StaticVariablesClass
+    {
+        private static int port;
+        private static string ipAddress;
+    }
+}
