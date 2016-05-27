5fea3206cc21c7daddc2f7cfdd8134ee4feeced1
sbecirovic <seila.becirovic@gmail.com>
2016-05-27 22:27:54 +0200

    Parser za filmove (:
    Signed-off-by: sbecirovic <seila.becirovic@gmail.com>

---
+++ Projekat/ShEM/ShEM/Helpers/MovieAPIParser.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using ShEM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace ShEM.Helpers
+{
+   public class MovieAPIParser
+    {
+        Movie film;
+        public async void getMovie(string search)
+        {
+            try
+            {
+                
+                WebClient client = new WebClient();
+                // Download string.
+                string query = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&r=json";
+                string value = client.DownloadString(query);
+                dynamic dyn = JsonConvert.DeserializeObject(value);
+                if (dyn != null)
+                {
+                    film.articleName = dyn.Title.ToString();
+                    int broj;
+                    Int32.TryParse(dyn.Runtime.ToString(),out broj);
+                    film.duration = broj;
+                    film.synopsys = dyn.Plot.ToString();
+                    film.director = dyn.Director.ToString();
+                    film.yearOfRelease = dyn.Year.ToString();
+                    film.genre = dyn.Genre.ToString();
+                    //  film.articleID = dyn.imdbID.ToString();
+                /*    string slika = dyn.Poster.ToString();
+                    WebClient webClient = new WebClient();
+                    still not sure how to do this
+                       webClient.DownloadFile(slika, film.image);
+                   */  
+
+
+                }
+
+                else
+                {
+                    var dialog = new MessageDialog("Nothing has been found. Please try again!");
+                    await dialog.ShowAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                var dialog = new MessageDialog("Nothing has been found. Please try again!");
+                await dialog.ShowAsync();
+            }
+
+        }
+    }
+}
+
---
+++ Projekat/ShEM/ShEM/Helpers/WebClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShEM.Helpers
+{
+    internal class WebClient
+    {
+        internal string DownloadString(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
